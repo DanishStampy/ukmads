15b81caf520d66320aaa108114eb4b51511d3e62
DanishStampy <a175139@siswa.ukm.edu.my>
2021-11-15 11:53:18 +0800

    Create table event and advertisement

---
+++ database/migrations/2021_11_15_033349_create_advertisements_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdvertisementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('advertisements', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255);
+            $table->string('type');
+            $table->float('price', 5, 2);
+            $table->string('seller_name', 255);
+            $table->string('contact');
+            $table->text('description');
+            $table->string('picture', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('advertisements');
+    }
+}
---
+++ database/migrations/2021_11_15_034204_create_events_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255);
+            $table->string('location');
+            $table->time('time');  # HH:MI
+            $table->date('date');   # YYYY-MM-DD
+            $table->string('organizer');
+            $table->string('contact');
+            $table->text('description');
+            $table->string('picture', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
