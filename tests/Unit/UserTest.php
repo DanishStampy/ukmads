1d12206e20b2b556103088f126e815654b264eac
Danish Irfan Shah <64722277+DanishStampy@users.noreply.github.com>
2022-05-17 02:51:34 +0800

    test

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_login_form()
+    {
+        $this->assertTrue(true);
+    }
+}
